608227187559833eec5f0186ed972360114ea136
Kyle Walkley <kylewalkley@gmail.com>
2025-10-05 22:33:09 -0400

    add Config type

---
+++ config/config.go
@@ -0,0 +1,19 @@
+package config
+
+import (
+	"os"
+)
+
+type Config struct {
+	GeminiAPIKey string
+	SpamhausFeedURL string
+	FeedLimit int
+}
+
+func Load() *Config {
+	return &Config{
+		GeminiAPIKey: os.Getenv("GEMINI_API_KEY"),
+		SpamhausFeedURL:   "https://www.spamhaus.org/drop/drop.txt",
+		FeedLimit: 5,
+	}
+}
